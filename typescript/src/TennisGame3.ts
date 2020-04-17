685a863913f4af6caad6ce6848be4de1e0e843e4
Pakawat Anekwiroj <pakawatanekwiroj@Pakawats-MacBook-Pro.local>
2020-04-17 23:29:31 +0700

    TEN-003 Refactor change variable name

--- typescript/src/TennisGame3.ts
+++ typescript/src/TennisGame3.ts
@@ -2,35 +2,34 @@ import { TennisGame } from './TennisGame';
 
 
 export class TennisGame3 implements TennisGame {
-  private p2: number = 0;
-  private p1: number = 0;
-  private p1N: string;
-  private p2N: string;
+  private p2Score: number = 0;
+  private p1Score: number = 0;
+  private p1Name: string;
+  private p2Name: string;
 
-  constructor(p1N: string, p2N: string) {
-    this.p1N = p1N;
-    this.p2N = p2N;
+  constructor(p1Name: string, p2Name: string) {
+    this.p1Name = p1Name;
+    this.p2Name = p2Name;
   }
 
   getScore(): string {
-    let s: string;
-    if (this.p1 < 4 && this.p2 < 4 && !(this.p1 + this.p2 === 6)) {
-      const p: string[] = ['Love', 'Fifteen', 'Thirty', 'Forty'];
-      s = p[this.p1];
-      return (this.p1 === this.p2) ? s + '-All' : s + '-' + p[this.p2];
+    if (this.p1Score < 4 && this.p2Score < 4 && !(this.p1Score + this.p2Score === 6)) {
+      const scoreText: string[] = ['Love', 'Fifteen', 'Thirty', 'Forty'];
+      const score = scoreText[this.p1Score];
+      return (this.p1Score === this.p2Score) ? score + '-All' : score + '-' + scoreText[this.p2Score];
     } else {
-      if (this.p1 === this.p2)
+      if (this.p1Score === this.p2Score)
         return 'Deuce';
-      s = this.p1 > this.p2 ? this.p1N : this.p2N;
-      return (((this.p1 - this.p2) * (this.p1 - this.p2)) === 1) ? 'Advantage ' + s : 'Win for ' + s;
+      const playerName = this.p1Score > this.p2Score ? this.p1Name : this.p2Name;
+      return (((this.p1Score - this.p2Score) * (this.p1Score - this.p2Score)) === 1) ? 'Advantage ' + playerName : 'Win for ' + playerName;
     }
   }
 
   wonPoint(playerName: string): void {
     if (playerName === 'player1')
-      this.p1 += 1;
+      this.p1Score += 1;
     else
-      this.p2 += 1;
+      this.p2Score += 1;
 
   }
 }
