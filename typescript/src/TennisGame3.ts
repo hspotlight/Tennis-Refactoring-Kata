9b6fb33a7645a40a4dae7b3fb3823b07042832c6
Pakawat Anekwiroj <pakawatanekwiroj@Pakawats-MacBook-Pro.local>
2020-04-18 11:23:53 +0700

    TEN-0003 Refactor extract function

--- typescript/src/TennisGame3.ts
+++ typescript/src/TennisGame3.ts
@@ -13,23 +13,22 @@ export class TennisGame3 implements TennisGame {
   }
 
   getScore(): string {
-    if (this.p1Score === this.p2Score && this.p1Score >= 3) {
-      return 'Deuce';
+    if (this.p1Score === this.p2Score) {
+      return this.p1Score >= 3 ? 'Deuce' : this.getScoreText(this.p1Score) + '-All';
     }
     else if (this.p1Score < 4 && this.p2Score < 4) {
-      const scoreText: string[] = ['Love', 'Fifteen', 'Thirty', 'Forty'];
-
-      if (this.p1Score === this.p2Score) {
-        return scoreText[this.p1Score] + '-All';
-      }
-
-      return scoreText[this.p1Score] + '-' + scoreText[this.p2Score];
+      return this.getScoreText(this.p1Score) + '-' + this.getScoreText(this.p2Score);
     }
     else {
       const playerName: string = this.p1Score > this.p2Score ? this.p1Name : this.p2Name;
       const diffResult = Math.abs(this.p1Score - this.p2Score);
       return (diffResult === 1 ? 'Advantage '  : 'Win for ') + playerName;
     }
+  }
+
+  private getScoreText(score: number) {
+    const scoreText: string[] = ['Love', 'Fifteen', 'Thirty', 'Forty'];
+    return scoreText[score];
   }
 
   wonPoint(playerName: string): void {
